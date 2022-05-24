5958be1799917a89508146a67c22011eb7095012
Valentina <Valentina_B@mail.ru>
2022-05-24 15:49:13 +0300

    Задача 59

---
+++ Zadacha54/Program.cs
@@ -0,0 +1,56 @@
+﻿/* Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы 
+каждой строки двумерного массива.*/
+
+int rows = Input("Введите колличество строк в массиве: ");
+int columns = Input("Введите колличество столбцов в массиве: ");
+int[,] array = new int[rows, columns];
+
+FillArray(rows, columns, array);
+PrintArray(rows, columns, array);
+
+for (int i = 0; i < rows; i++)
+{
+    for (int j = 0; j < columns; j++)
+    {
+        for (int k = 0; k < columns; k++)
+        {
+            if (array[i, j] <= array[i, k]) continue;
+            int temporary = array[i, j];
+            array[i, j] = array[i, k];
+            array[i, k] = temporary;
+        }
+    }
+}
+
+ Console.WriteLine();
+ PrintArray(rows, columns, array);
+
+int Input(string output)
+{
+    Console.WriteLine(output);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void FillArray(int rows, int columns, int[,] array)
+{
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            array[i, j] = new Random().Next(0,10);
+        }
+    }
+}
+
+void PrintArray(int rows, int columns, int[,] array)
+{
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine(); 
+    }
+}
+
---
+++ Zadacha59/Program.cs
@@ -0,0 +1,81 @@
+﻿/* Задача 59: Задайте двумерный массив из целых чисел. Напишите программу, которая удалит строку и столбец, на 
+пересечении которых расположен наименьший элемент массива.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+5 2 6 7
+Наименьший элемент - 1, на выходе получим
+следующий массив:
+9 4 2
+2 2 6
+3 4 7*/
+
+
+int rows = Input("Введите количество строк в массиве: ");
+int columns = Input("Введите количество столбцов в массиве: ");
+int[,] array = new int[rows, columns];
+
+FillArray(rows, columns, array);
+PrintArray(rows, columns, array);
+
+int minNumber = array[0, 0]; //Минимальные значения
+int minRows = 0;
+int minColumns = 0;
+for (int i = 0; i < rows; i++)
+{
+    for (int j = 0; j < columns; j++)
+    {
+        if (array[i, j] <= minNumber)
+        {
+            minNumber = array[i, j];
+            minRows = i;
+            minColumns =j;
+        }
+    }
+}
+Console.WriteLine("Наименьший элемент: " + minNumber);
+
+for (int i = 0; i < rows; i++) // Преобразованный массив
+{
+    for (int j = 0; j < columns; j++)
+    {
+        {
+            if ((i == minRows) || (j == minColumns))
+                continue;
+            else
+                Console.Write(array[i, j] + " ");
+        }
+    }
+    Console.WriteLine();
+}
+ 
+
+int Input (string output)
+{
+    Console.WriteLine(output);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void FillArray(int rows, int columns, int[,] array)
+{
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            array[i, j] = new Random().Next(0,100);
+        }
+    }
+}
+
+void PrintArray(int rows, int columns, int[,] array)
+{
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine(); 
+    }
+}
